0d6e441b8527424f80b84794c2164d5f612369a1
action <xingdong1117@126.com>
2025-03-31 14:57:46 +0800

    fix:RabbitMQ声明式配置使用

--- src/main/java/com/action/project/RabbitMQConfig.java
+++ src/main/java/com/action/project/RabbitMQConfig.java
@@ -1,5 +1,9 @@
 package com.action.project;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
@@ -31,5 +35,70 @@ public class RabbitMQConfig {
         rabbitAdmin.setAutoStartup(true);
         return rabbitAdmin;
     }
+
+    /**
+     * 针对消费者配置
+     * 1. 设置交换机类型
+     * 2. 将队列绑定到交换机
+     * Fanout: 将消息分发到所有的绑定队列，无routingkey的概念
+     * HeadersExchange: 通过添加属性key-value匹配
+     * DirectExchange:按照routingkey分发到指定队列
+     * TopicExchange:多关键字匹配
+     * @return
+     */
+    @Bean
+    public TopicExchange exchange001(){
+        return new TopicExchange("topic001",true,false);
+    }
+
+    @Bean
+    public Queue queue001(){
+        //队列持久
+        return new Queue("queue001",true);
+    }
+
+    @Bean
+    public Binding binding001(){
+        return BindingBuilder.bind(queue001()).to(exchange001()).with("spring.*");
+    }
+
+    @Bean
+    public TopicExchange exchange002(){
+        return new TopicExchange("topic002",true,false);
+    }
+
+    @Bean
+    public Queue queue002(){
+        //队列持久
+        return new Queue("queue002",true);
+    }
+
+    @Bean
+    public Binding binding002(){
+        return BindingBuilder.bind(queue002()).to(exchange002()).with("rabbit.*");
+    }
+
+
+    @Bean
+    public Queue queue003(){
+        //队列持久
+        return new Queue("queue003",true);
+    }
+
+    @Bean
+    public Binding binding003(){
+        return BindingBuilder.bind(queue003()).to(exchange001()).with("mq.*");
+    }
+
+    @Bean
+    public Queue queue_image(){
+        return new Queue("image_queue",true);
+    }
+
+    @Bean
+    public Queue queue_pdf(){
+        return new Queue("pdf_queue",true);
+    }
+
 
 }
